b4fde266dd1be3a0154584b0e34b504e79ee8087
Sugaray Tega <tegasugarayts@gmail.com>
2023-04-30 16:46:29 +0100

    Changed AOS to css animation so that buttons can show. As they tend to disappear if they are in the bottom of the page

--- src/components/CuiButton.vue
+++ src/components/CuiButton.vue
@@ -7,13 +7,23 @@ const props = defineProps({
 </script>
 
 <template>
-	<button data-aos="fade-in" v-ripple="'rgba(255,255,255,.6)'" :class="'cui-button-'.concat(props.type)" class="cui-button p-2 rounded">
+	<button v-ripple="'rgba(255,255,255,.6)'" :class="'cui-button-'.concat(props.type)" class="cui-button p-2 rounded">
 		<slot></slot>
 	</button>
 </template>
 
 <style scoped>
+	@keyframes fade-in {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+
 	.cui-button {
+		animation: fade-in 100ms ease-in;
 		color: white;
 		background-color: var(--cui-blue);
 		font-weight: light;
