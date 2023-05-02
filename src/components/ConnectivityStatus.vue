c6684cd4baf105dd8a5c1bce8660a110c00fad3a
Sugaray Tega <tegasugarayts@gmail.com>
2023-05-02 10:08:42 +0100

    Added a connectivity status component to disable the application from being used when you are offline

---
+++ src/components/ConnectivityStatus.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { useAppStore } from '@/store/app.store';
+import { storeToRefs } from 'pinia';
+const appStore = useAppStore();
+const { online } = storeToRefs(appStore);
+</script>
+<template>
+	<div v-if="!online">
+		
+		You are offline
+		<br>
+		:-(
+		
+	</div>
+</template>
+
+<style scoped>
+div {
+	position: fixed;
+	top: 0;
+	right: 0;
+	left: 0;
+	bottom: 0;
+	background-color: var(--cui-white);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	z-index: 10000000000000000000000000000000000000000000;
+	font-weight: bolder;
+	font-size: 4rem;
+	text-align: center;
+}
+</style>
