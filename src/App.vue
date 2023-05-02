af9ed9004b6a0012d25af8b64fac461b269e7832
Sugaray Tega <tegasugarayts@gmail.com>
2023-05-02 10:12:06 +0100

    Implemented connectivity status in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
 import AppAlert from '@/components/AppAlert';
-import ProcessLoader from '@/components/ProcessLoader'
+import ProcessLoader from '@/components/ProcessLoader';
+import ConnectivityStatus from '@/components/ConnectivityStatus';
+import { useAppStore } from '@/store/app.store';
+import { storeToRefs } from 'pinia';
+
+const appStore = useAppStore();
+const { online } = storeToRefs(appStore);
+
+window.ononline = ()=>{
+	online.value = true;
+}
+
+window.onoffline = ()=>{
+	online.value = false;
+}
 </script>
 <template>
   <app-alert></app-alert>
   <process-loader></process-loader>
+  <connectivity-status></connectivity-status>
   <router-view/>
 </template>
